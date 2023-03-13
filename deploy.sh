c9efde37512f0c3dd56ab1deb9d56af643cb750d
vishnuac57 <113792713+vishnuac57@users.noreply.github.com>
2023-03-13 10:46:02 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
  sh 'sudo docker login -u vishnuac1999 -p Vishnuselvam@1999'
- sh 'sudo docker tag reactapp:latest vishnuac1999/reactapp
- sh 'sudo docker push vishnuac1999/reactapp'
+ sh 'sudo docker tag reactapp:latest vishnuac1999/dev:latest'
+ sh 'sudo docker push vishnuac1999/dev:latest'
  echo "image push at  dev dockerhub"
